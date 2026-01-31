87cb303e41e32cf50181764b1f1d95bc9e71802c
Ravel Rodrigues Pereira <ravelrodriguesp@gmail.com>
2026-01-31 11:29:09 -0300

    Add initial header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from "react";
-import Login from "./pages/Login";
+import React from 'react';
+import Login from './pages/Login';
 
-import GlobalStyle from "./styles/GlobalStyles";
+import GlobalStyle from './styles/GlobalStyles';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
+      <Header />
       <Login />
       <GlobalStyle />
     </>
